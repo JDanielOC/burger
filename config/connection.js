13fd98f85f08fbfbf19265bc6557e8818a3396a6
Jason <vivanaranja@gmail.com>
2018-01-04 23:10:50 -0800

    more setup

---
+++ config/connection.js
@@ -0,0 +1,23 @@
+// Require mysql
+var mysql = require("mysql");
+
+// Set up our connection information
+var connection = mysql.createConnection({
+    port: 3000,
+    host: "localhost",
+    user: "root",
+    password: "5eLUrWj=$J",
+    database: "burgers_db"
+});
+
+// Connect to the database
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
