2de0a71e0ad029b405848704c2f82b61cfdaf604
Jason <vivanaranja@gmail.com>
2018-01-08 00:06:44 -0800

    orm stuff

--- config/orm.js
+++ config/orm.js
@@ -15,7 +15,7 @@ function objToSql(ob) {
 
     // loop through the keys and push the key/value as a string int arr
     for (var key in ob) {
-        if (Object.hasOwnProperty.call(ob, key)) {
+        if (ob.hasOwnProperty.call(ob, key)) {
             // if string with spaces, add quotations)
             if (typeof value === "string" && value.indexOf(" ") >= 0) {
                 value = "'" + value + "'";
@@ -29,6 +29,7 @@ function objToSql(ob) {
 }
 
 var orm = {
+    // selectAll grabs all from burgers table
     selectAll: function(tableInput, cb) {
         var queryString = 'SELECT * FROM ' + tableInput + ';';
         connection.query(queryString, function(err, result) {
@@ -38,6 +39,7 @@ var orm = {
             cb(result);
         });
     },
+    // insertOne adds a burger to the table
     insertOne: function(table, cols, vals, cb) {
         var queryString = 'INSERT INTO ' + table;
 
@@ -58,6 +60,7 @@ var orm = {
             cb(result);
         });
     },
+    // updateOne changes from devoured false to devoured true
     updateOne: function(table, objColVals, condition, cb) {
         var queryString = 'UPDATE ' + table;
 
@@ -67,6 +70,7 @@ var orm = {
         queryString += condition;
 
         console.log(queryString);
+        
         connection.query(queryString, function(err, result) {
             if (err) {
                 throw err;
