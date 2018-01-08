b4aba4df238cba7f68b7336a6ef2ec530f711c5a
Jason <vivanaranja@gmail.com>
2018-01-08 09:59:34 -0800

    he ro ku

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,23 @@
 // Require mysql
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-    port: 3306,
-    host: 'localhost',
-    user: 'root',
-    password: '5eLUrWj=$J',
-    database: 'burgers_db'
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: '5eLUrWj=$J',
+        database: 'burgers_db'
+    });
+};
+
 
 // Connect to the database
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) {
         console.error('error connecting: ' + err.stack);
         return;
